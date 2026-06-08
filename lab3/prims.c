2a8b0189a03936c9e8ec178810986ae749c50d3a
r-vandanaa <rvandanaa.cs24@bmsce.ac.in>
2026-06-08 22:33:11 +0530

    Refactor Prim's algorithm implementation

--- lab3/prims.c
+++ lab3/prims.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #define MAX 10
-#define INT_MAX 10000
+#define INF 9999
 
-void prims(int n, int cost[MAX][MAX]){
+void prims(int n, int cost[MAX][MAX]) {
     int visited[MAX] = {0};
-    visited[0] = 1;
+    visited[0] = 1;  
+    int total = 0;
 
-    int num_of_edges = 0;
-    int min_cost = 0;
-    int u = -1, v = -1;
+    printf("Edges in MST:\n");
 
-    printf("Edges:");
+    for (int edges = 0; edges < n - 1; edges++) {
+        int u = -1, v = -1;
+        int min = INF;
 
-    while(num_of_edges < n - 1){
-        int min = INT_MAX;
-
-        for(int i = 0; i < n; i++){
-            if(visited[i] == 1){
-                for(int j = 0; j < n; j++){
-                    if(visited[j] == 0 && cost[i][j] < min){
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                for (int j = 0; j < n; j++) {
+                    if (!visited[j] && cost[i][j] < min) {
                         min = cost[i][j];
                         u = i;
                         v = j;
@@ -27,36 +25,32 @@ void prims(int n, int cost[MAX][MAX]){
             }
         }
 
-        if(min == INT_MAX){
-            printf("\nGraph is not connected\n");
-            return;
+        if (u != -1 && v != -1) {
+            printf("%d -- %d  (weight %d)\n", u, v, min);
+            visited[v] = 1;
+            total += min;
         }
-
-        printf("\n%d --> %d", u, v);
-        min_cost += min;
-        visited[v] = 1;
-        num_of_edges++;
     }
 
-    printf("\nTotal weight of graph: %d\n", min_cost);
+    printf("Total weight = %d\n", total);
 }
 
-int main(){
+int main() {
     int n;
+    int cost[MAX][MAX];
+
     printf("Enter number of vertices: ");
     scanf("%d", &n);
 
-    int cost[MAX][MAX];
-
     printf("Enter adjacency matrix:\n");
-    for(int i = 0; i < n; i++){
-    for(int j = 0; j < n; j++){
-        scanf("%d", &cost[i][j]);
-        if(i != j && cost[i][j] == 0){
-            cost[i][j] = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &cost[i][j]);
+            if (i != j && cost[i][j] == 0) {
+                cost[i][j] = INF; 
+            }
         }
     }
-}
 
     prims(n, cost);
     return 0;
